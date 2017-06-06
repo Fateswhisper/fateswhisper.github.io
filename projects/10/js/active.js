abccdda7070253c3a7c8abc97256ef5eb878f939
fateswhisper <fatesvvhisper@gmail.com>
2017-06-06 22:06:38 +0300

    Misc

--- projects/10/js/active.js
+++ projects/10/js/active.js
@@ -1,5 +1,5 @@
 $(function () {                                 // когда страница загружена
-    $('.nav-menu-items a').each(function () {   // проходим по нужным нам ссылками
+    $('.navigation-links a').each(function () {   // проходим по нужным нам ссылками
         var location = window.location.href     // переменная с адресом страницы
         var link = this.href                    // переменная с url ссылки
         var result = location.match(link);      // результат возвращает объект если совпадение найдено и null при обратном
